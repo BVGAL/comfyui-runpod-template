02cfcc89f3796f5bf445390659c3a7770d6da6b2
BVGAL <166797001+BVGAL@users.noreply.github.com>
2025-09-26 07:23:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,17 @@ set -e
 
 cd /workspace
 
-# Se la cartella non esiste, clona comfyUI
+# Clona o aggiorna ComfyUI
 if [ ! -d "ComfyUI" ]; then
     git clone https://github.com/comfyanonymous/ComfyUI.git
 fi
-
-# Aggiorna sempre ComfyUI
 cd ComfyUI
 git pull origin master
 
-# Installa/aggiorna requirements
+# Installa/aggiorna requirements di ComfyUI
 pip install -r requirements.txt
 
-# ComfyUI Manager (nella cartella custom_nodes)
+# Gestione ComfyUI Manager
 cd custom_nodes
 if [ ! -d "ComfyUI-Manager" ]; then
     git clone https://github.com/ltdrdata/ComfyUI-Manager.git
@@ -26,5 +24,5 @@ git pull origin main
 # Torna alla root ComfyUI
 cd /workspace/ComfyUI
 
-# Avvia comfyUI alla porta 8188
+# Avvia ComfyUI sulla porta 8188
 exec python3 main.py --listen 0.0.0.0 --port 8188
